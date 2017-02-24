8e8682835875c0c99d8fed0aa20f657d7fbf83ba
cnzsb <cnzsb@foxmail.com>
2017-02-24 15:02:41 +0800

    use event handlers

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -4,8 +4,11 @@ export default class Clock extends Component {
   constructor() {
     super();
     this.state = {
-      date: new Date()
+      date: new Date(),
+      isToggleOn: true
     };
+
+    // this.handleClick = this.handleClick.bind(this) // 方法1 使用 bind 绑定 this
   }
 
   componentDidMount() {
@@ -25,11 +28,20 @@ export default class Clock extends Component {
     })
   }
 
+  handleClick() {  //  方法2 使用箭头函数绑定 this, 方法3同理，在 dom 中使用
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }))
+  }
+
   render() {
     return (
       <div>
         <h3>Use State In React</h3>
         <h4 className="clock">It is {this.state.date.toLocaleTimeString()}.</h4>
+        <button onClick={e => this.handleClick(e)}>
+          {this.state.isToggleOn ? 'ON' : 'OFF'}
+        </button>
       </div>
     )
   }
